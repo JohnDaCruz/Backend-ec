3c07bbf8b788591024a4477573113b9dde87e744
JohnDaCruz <102554786+JohnDaCruz@users.noreply.github.com>
2024-05-05 21:13:19 -0300

    Fist commit

---
+++ src/main/java/john/backendec/BackendEcApplication.java
@@ -0,0 +1,13 @@
+package john.backendec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEcApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/john/backendec/BackendEcApplicationTests.java
@@ -0,0 +1,13 @@
+package john.backendec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEcApplicationTests{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
